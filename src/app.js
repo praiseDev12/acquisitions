b05be2f3f07bc2e2d0d9befd66e58edbd3ed65d4
Ezeofor PraiseGod <senitenttechnology@gmail.com>
2025-10-03 16:51:02 +0100

    implemented auth routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import logger from '#config/logger.js';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
 
 const app = express();
 
@@ -23,5 +24,19 @@ app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions API!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(100).json({ message: 'Acquisitions API is running!' });
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
